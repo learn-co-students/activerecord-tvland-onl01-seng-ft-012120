deb7de7707459dc88826453fac76f4ac26f8c5ed
thedannysoto <87dsoto@gmail.com>
2020-02-14 16:55:23 -0600

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,20 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor 
+    belongs_to :show
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
+
+    def build_show(arg)
+        @@new_show = Show.new(arg)
+        @@new_show.characters << self
+    end
+
+    def self.build_network(arg)
+        Show.build_network(arg, @@new_show)
+    end
+
+#     rick = Character.new(:name => "Rick Grimes")
+#   rick.build_show(:name => "The Walking Dead").build_network(:call_letters => "AMC")
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map do |actor|
+            actor.first_name + " " + actor.last_name
+        end
+    end
+
+    def self.build_network(arg, x=self)
+        network = Network.new(arg)
+        network.shows << x
+    end
+
 end
