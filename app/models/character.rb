4373d8593e3bede03685e8ba979c8045028ee9a2
Patrick Grady <patickrgrady81@gmail.com>
2020-02-18 10:32:54 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  # rick.build_show(:name => "The Walking Dead").build_network(:call_letters => "AMC")
+  # def build_show
+
+  # end
 end
