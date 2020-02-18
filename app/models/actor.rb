461c87bb32ebefd46fe06bf2fcf23ddc8392169f
reddevilcero <reddevil_cero@hotmail.com>
2020-02-18 15:35:23 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows , through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each do |character|
+      show_id = character.show_id
+      show = self.shows.collect{|show|show.name if show.id == 1}[0]
+      roles << "#{character.name} - #{show}"
+    end
+    roles
+  end
 end
