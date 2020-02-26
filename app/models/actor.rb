efc6bb36d68296c64416a05159ccc4571481f689
andrew041994@gmail.com <andrew041994@gmail.com>
2020-02-26 13:39:47 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+      characters = self.characters.collect{|c| c.name}
+      shows = self.shows.collect{|s| s.name}
+      roles = characters.zip(shows).join(" - ")
+      roles
+    end  
 end
