fefcb2f4a4e1d3c2be23d735ad899f2f625532cc
nisticism <nistacular@gmail.com>
2020-03-23 01:52:13 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,33 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        name = self.first_name + " " + self.last_name
+        name
+    end
+
+    def list_roles
+        roles = []
+        x = 0
+        while x <= self.characters.length
+            if self.characters.find_by_id(x)
+                add_this = self.characters.find_by_id(x).name + " - " + self.characters.find_by_id(x).show.name
+                roles << add_this
+            end
+            x += 1
+        end
+        # self.characters do |character|
+        #     add_this = ""
+        #     #if character
+        #         add_this += character.name
+        #         add_this += " - "
+        #         add_this += character.show
+        #     #end
+        #     roles << add_this
+        # end
+        roles 
+    end
+            
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        actors = []
+        x = 0
+        while x <= self.characters.length
+            if self.characters.find_by_id(x)
+                add_this = self.characters.find_by_id(x).actor.first_name + " " + self.characters.find_by_id(x).actor.last_name
+                actors << add_this
+            end
+            x += 1
+        end
+        actors
+    end
+
 end
